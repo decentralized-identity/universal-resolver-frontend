552349b1aafc7149dfe5f7171da7aa4e12b8dfa1
Markus Sabadello <markus@danubetech.com>
2018-02-28 09:47:06 +0100

    Small code change.

--- src/components/result/PublicKey.jsx
+++ src/components/result/PublicKey.jsx
@@ -8,8 +8,8 @@ export class PublicKey extends Component {
 		var publicKey;
 		if (this.props.publicKeyBase64) publicKey = this.props.publicKeyBase64;
 		else if (this.props.publicKeyBase58) publicKey = this.props.publicKeyBase58;
-		else if (this.props.publicKeyPem) publicKey = this.props.publicKeyPem;
 		else if (this.props.publicKeyHex) publicKey = this.props.publicKeyHex;
+		else if (this.props.publicKeyPem) publicKey = this.props.publicKeyPem;
 		else publicKey = '(no value)';
         return (
         	<Card className='publickey'>
