684efaee2157e46fd9de59b63a63fd30d6482140
Markus Sabadello <markus@danubetech.com>
2020-03-12 10:37:14 +0100

    fix: Fix did:kilt example.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- src/components/Resolver.jsx
+++ src/components/Resolver.jsx
@@ -40,7 +40,7 @@ export class Resolver extends Component {
 			'did:ccp:ceNobbK6Me9F5zwyE3MKY88QZLw',
 			'did:work:2UUHQCd4psvkPLZGnWY33L',
 			'did:ont:AN5g6gz9EoQ3sCNu7514GEghZurrktCMiH',
-			'did:kilt:5CDct4QDpQYfAVDrskNuiEdXyiE38oPfTHEJ65ZLSpz9WasE',
+			'did:kilt:5GFs8gCumJcZDDWof5ETFqDFEsNwCsVJUj2bX7y4xBLxN5qT',
 			'did:web:uport.me',
 			'ssi.labs.nic.at',
 			'did:evan:testcore:0x126E901F6F408f5E260d95c62E7c73D9B60fd734'
