a06ccc736c6d9353e645deac5ea63ba06754a680
Markus Sabadello <markus@danubetech.com>
2020-06-17 11:34:31 +0200

    ui: Additional warning in footer.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
-import { Segment, Icon } from 'semantic-ui-react'
+import { Button, Segment, Icon } from 'semantic-ui-react'
+
+import WarningPopup from './WarningPopup';
 
 export class Footer extends Component {
 
 	render() {
+		const link = (
+			<a>here</a>
+		);
 		return (
 			<Segment className='footer'>
 				<Icon className="blue info circle" />
 				See <a href="https://github.com/decentralized-identity/universal-resolver/">here</a> for more information about the Universal Resolver.
+				Warning - this is a non-production service. See <WarningPopup trigger={link} />.
 			</Segment>
 		);
 	}
--- src/components/WarningButton.jsx
+++ src/components/WarningButton.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
-import { Item, Button, Modal, Header, Divider, Icon } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
+
+import WarningPopup from './WarningPopup';
 
 export class WarningButton extends Component {
 
@@ -10,36 +11,7 @@ export class WarningButton extends Component {
 			<Button className="warning-button orange icon right floated"><Icon className="exclamation triangle" /></Button>
 		);
 		return (
-			<Modal trigger={button}>
-				<Modal.Header>
-					<Icon className="orange exclamation triangle" />
-					Warning
-				</Modal.Header>
-				<Modal.Content>
-					<Modal.Description>
-						<p>Before using the Universal Resolver, make sure you understand two things:</p>
-						<Header>1. It's experimental</Header>
-						<p>Decentralized Identifiers (DIDs) are currently being developed by the <a href="https://www.w3.org/2019/did-wg/">
-						W3C Decentralized Identifier Working Group</a>. The <a href="https://w3c.github.io/did-spec/">DID</a> and <a href="https://w3c-ccg.github.io/did-resolution/">DID
-						Resolution</a> specifications are
-						constantly evolving, and so is the Universal
-						Resolver implementation. Understand that
-						everything about DIDs and the Universal Resolver is subject to change at any time.</p>
-						<Header>2. Don't rely on the public instance</Header>
-						<p>DIDs and DID Resolution are by definition meant to be decentralized. Under the <strong>uniresolver.io</strong> domain
-						name we are running an instance of the Universal Resolver, but you should not rely on this
-						instance for anything other than development and non-critical demos. From your perspective,
-						you should treat <strong>uniresolver.io</strong> as an untrusted third party that could theoretically manipulate
-						DID Resolution results. Instead, you should run your own instance of the Universal Resolver
-						or other DID Resolution tools to integrate with your applications and services.</p>
-						<Divider/>
-						<p>
-						<Icon className="blue info circle" />
-						See <a href="https://github.com/decentralized-identity/universal-resolver/">here</a> for
-						more information about the Universal Resolver.</p>
-					</Modal.Description>
-				</Modal.Content>
-			</Modal>
+			<WarningPopup trigger={button} />
 		);
 	}
 }
